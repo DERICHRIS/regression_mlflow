24b9de8b9b1d09f88e188280ba6ce35a6ac846c1
DERICHRIS <dericvictor2@gmail.com>
2025-02-28 20:19:34 +0530

    knn evaluated

--- knn3.py
+++ knn3.py
@@ -16,40 +16,29 @@ X_cleaned = X.dropna()
 # Remove corresponding rows in the target variable (y) to keep alignment
 y_cleaned = y[X_cleaned.index]
 
-# Now X_cleaned and y_cleaned contain no NaN values
-print(X_cleaned.head())
-print(y_cleaned.head())
-
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-
-model=LinearRegression()
-
+from sklearn.neighbors import KNeighborsRegressor
 import mlflow
 import mlflow.sklearn
+from sklearn.metrics import mean_squared_error,r2_score
 
-mlflow.set_tracking_uri('http://127.0.0.1:5000')
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 
 with mlflow.start_run():
+    model=KNeighborsRegressor(n_neighbors=25)
     X_train,X_test,y_train,y_test=train_test_split(X_cleaned,y_cleaned,test_size=0.2,random_state=42)
+
     model.fit(X_train,y_train)
     y_pred=model.predict(X_test)
 
-    mse=mean_squared_error(y_test,y_pred)
+    sme=mean_squared_error(y_test,y_pred)
     r2=r2_score(y_test,y_pred)
 
-    mlflow.log_param("model","linear model")
-    mlflow.log_metric("mean square error",mse)
+    mlflow.log_param("model","KNN3")
+    mlflow.log_metric("mean square error",sme)
     mlflow.log_metric("r2",r2)
 
-    mlflow.sklearn.log_model(model,"linear model")
+    mlflow.sklearn.log_model(model,"KNN3")
 
-    print(f"mse:{mse}")
+    print(f"sme :{sme}")
     print(f"r2 : {r2}")
-
-
-
-
-
-
